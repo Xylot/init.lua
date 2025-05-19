fbf6101fb91683ae55f60411c045c660452c7254
Xylot <Joseph.meriam@gmail.com>
2025-05-19 01:01:19 -0500

    Add hardtime.nvim

--- plugin/20_plugins.lua
+++ plugin/20_plugins.lua
@@ -1,15 +1,27 @@
+
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 later(function()
-  add({
-    source = 'nvim-treesitter/nvim-treesitter',
-    checkout = 'master',
-    monitor = 'main',
-    hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
-  })
+    add({
+        source = 'nvim-treesitter/nvim-treesitter',
+        checkout = 'master',
+        monitor = 'main',
+        hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+    })
 
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'lua', 'vimdoc', 'yaml', 'python', 'rust', 'typescript'},
-    highlight = { enable = true },
-  })
+    require('nvim-treesitter.configs').setup({
+        ensure_installed = { 'lua', 'vimdoc', 'yaml', 'python', 'rust', 'typescript' },
+        highlight = { enable = true },
+    })
+end)
+
+later(function()
+    add({
+        source = 'm4xshen/hardtime.nvim',
+        depends = {
+            'MunifTanjim/nui.nvim',
+        }
+    })
+
+    require('hardtime').setup({})
 end)
